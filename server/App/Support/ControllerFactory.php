2033a6b3371d6e6244c2208ade4fc88dad71063c
jackhr <jc2o@mac.com>
2026-03-19 20:58:06 -0400

    Wire controller factory to new repository set

--- server/App/Support/ControllerFactory.php
+++ server/App/Support/ControllerFactory.php
@@ -14,7 +14,10 @@ use App\Controllers\TaxiRequestController;
 use App\Controllers\VehicleController;
 use App\Controllers\VehicleDiscountController;
 use App\Controllers\VehicleRequestController;
-use App\Repositories\RentalRepository;
+use App\Repositories\AddOnRepository;
+use App\Repositories\BookingRepository;
+use App\Repositories\TaxiRequestRepository;
+use App\Repositories\VehicleRepository;
 use App\Services\AddOnService;
 use App\Services\ContactService;
 use App\Services\ContactInfoService;
@@ -25,10 +28,15 @@ use App\Services\TaxiRequestService;
 use App\Services\VehicleDiscountService;
 use App\Services\VehicleService;
 use App\Services\VehicleRequestService;
+use PDO;
 
 final class ControllerFactory
 {
-    private static ?RentalRepository $rentalRepository = null;
+    private static ?PDO $pdo = null;
+    private static ?AddOnRepository $addOnRepository = null;
+    private static ?BookingRepository $bookingRepository = null;
+    private static ?TaxiRequestRepository $taxiRequestRepository = null;
+    private static ?VehicleRepository $vehicleRepository = null;
 
     public static function contactController(): ContactController
     {
@@ -37,57 +45,111 @@ final class ControllerFactory
 
     public static function addOnController(): AddOnController
     {
-        return new AddOnController(new AddOnService(self::rentalRepository()));
+        return new AddOnController(new AddOnService(self::addOnRepository()));
     }
 
     public static function contactInfoController(): ContactInfoController
     {
-        return new ContactInfoController(new ContactInfoService(self::rentalRepository()));
+        return new ContactInfoController(new ContactInfoService(self::bookingRepository()));
     }
 
     public static function orderRequestController(): OrderRequestController
     {
-        return new OrderRequestController(new OrderRequestService(self::rentalRepository()));
+        return new OrderRequestController(new OrderRequestService(self::bookingRepository()));
     }
 
     public static function taxiController(): TaxiController
     {
-        return new TaxiController(new TaxiService(self::rentalRepository()));
+        return new TaxiController(new TaxiService(self::taxiRequestRepository()));
     }
 
     public static function taxiRequestController(): TaxiRequestController
     {
-        return new TaxiRequestController(new TaxiRequestService(self::rentalRepository()));
+        return new TaxiRequestController(new TaxiRequestService(self::taxiRequestRepository()));
     }
 
     public static function reservationController(): ReservationController
     {
-        return new ReservationController(new ReservationService(self::rentalRepository()));
+        return new ReservationController(
+            new ReservationService(
+                self::vehicleRepository(),
+                self::addOnRepository()
+            )
+        );
     }
 
     public static function vehicleController(): VehicleController
     {
-        return new VehicleController(new VehicleService(self::rentalRepository()));
+        return new VehicleController(new VehicleService(self::vehicleRepository()));
     }
 
     public static function vehicleDiscountController(): VehicleDiscountController
     {
-        return new VehicleDiscountController(new VehicleDiscountService(self::rentalRepository()));
+        return new VehicleDiscountController(new VehicleDiscountService(self::vehicleRepository()));
     }
 
     public static function vehicleRequestController(): VehicleRequestController
     {
-        return new VehicleRequestController(new VehicleRequestService(self::rentalRepository()));
+        return new VehicleRequestController(
+            new VehicleRequestService(
+                self::bookingRepository(),
+                self::vehicleRepository()
+            )
+        );
     }
 
-    private static function rentalRepository(): RentalRepository
+    private static function pdo(): PDO
     {
-        if (self::$rentalRepository instanceof RentalRepository) {
-            return self::$rentalRepository;
+        if (self::$pdo instanceof PDO) {
+            return self::$pdo;
         }
 
-        self::$rentalRepository = new RentalRepository(\Database::connection());
+        self::$pdo = \Database::connection();
 
-        return self::$rentalRepository;
+        return self::$pdo;
+    }
+
+    private static function addOnRepository(): AddOnRepository
+    {
+        if (self::$addOnRepository instanceof AddOnRepository) {
+            return self::$addOnRepository;
+        }
+
+        self::$addOnRepository = new AddOnRepository(self::pdo());
+
+        return self::$addOnRepository;
+    }
+
+    private static function bookingRepository(): BookingRepository
+    {
+        if (self::$bookingRepository instanceof BookingRepository) {
+            return self::$bookingRepository;
+        }
+
+        self::$bookingRepository = new BookingRepository(self::pdo());
+
+        return self::$bookingRepository;
+    }
+
+    private static function taxiRequestRepository(): TaxiRequestRepository
+    {
+        if (self::$taxiRequestRepository instanceof TaxiRequestRepository) {
+            return self::$taxiRequestRepository;
+        }
+
+        self::$taxiRequestRepository = new TaxiRequestRepository(self::pdo());
+
+        return self::$taxiRequestRepository;
+    }
+
+    private static function vehicleRepository(): VehicleRepository
+    {
+        if (self::$vehicleRepository instanceof VehicleRepository) {
+            return self::$vehicleRepository;
+        }
+
+        self::$vehicleRepository = new VehicleRepository(self::pdo());
+
+        return self::$vehicleRepository;
     }
 }
